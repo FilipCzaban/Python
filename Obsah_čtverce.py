efaad013e1478d9626c18e1e71836090a6c303c6
FilipCzaban <betkavodakova@seznam.cz>
2025-02-09 23:17:52 +0100

    Add files via upload

---
+++ Obsah_čtverce.py
@@ -0,0 +1,5 @@
+def fce(a,b,c,d):
+    obsah = (a*a)
+    return obsah
+S = (fce(a = 5,b = 5,c= 5,d = 5))
+print(f"Obsah = {S}cm²")
---
+++ Obvod_Čtverce.py
@@ -0,0 +1,5 @@
+def fce(a,b,c,d):
+    obvod = (a+b+c+d)
+    return obvod 
+o = (fce(a = 5,b = 5,c= 5,d = 5))
+print(f"Obvod = {o} cm")
