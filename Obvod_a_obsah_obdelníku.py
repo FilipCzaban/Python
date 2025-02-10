6db3cdf01dcbbeed2043b0fafcf4d250717dea33
FilipCzaban <betkavodakova@seznam.cz>
2025-02-10 20:52:24 +0100

    Add files via upload

---
+++ Obvod_a_obsah_obdelníku.py
@@ -0,0 +1,10 @@
+def obvod(a, b):
+    return (2*(a+b))
+
+def obsah(a, b):
+    return (a*b)
+    
+O = (obvod(a = 5, b = 3))
+S = (obsah(a = 5, b = 3))
+print(f"Obsah = {S} cm²")
+print(f"Obvod = {O} cm")
