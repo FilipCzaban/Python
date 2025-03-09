109f0594115ff92e6436fba44769966d48256126
FilipCzaban <betkavodakova@seznam.cz>
2025-03-09 19:12:29 +0100

    Add files via upload

---
+++ Procenta.py
@@ -0,0 +1,4 @@
+x = float(input("Zadej hondotu ze který budeš chtít počítat procenta:"))
+p = float(input("Zadej kolik procent chceš vědět z x:"))
+Percent = (x / 100) * p
+print(f"procenta/o z {x} je {Percent} ")
